a33eb0d6601da6ff765ff1e51943686aa2dca086
NakePranob <pranob.onphaeng@gmail.com>
2025-03-18 21:50:36 +0700

    d

--- components/Captcha.vue
+++ components/Captcha.vue
@@ -7,10 +7,10 @@ const auth = useAuthStore();
 <template>
     <UModal id="captcha-modal" v-model="auth.captchaIsShow" prevent-close>
         <div class="relative min-h-96 px-4 py-16 bg-white rounded-lg">
-            <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -z-10">
                 <UCircular size="24" color="#E91C21"/>
             </div>
-            <div id="captcha-container" class="bg-white"></div>
+            <div id="captcha-container" class="absolute bg-white"></div>
         </div>
     </UModal>
 </template>
